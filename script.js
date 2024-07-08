a4e34b087cb7d6ac064b7ef910d27abdfa6933fa
CS <chadi.samadi96@gmail.com>
2024-07-08 23:47:48 +0300

    adding API

---
+++ script.js
@@ -0,0 +1,11 @@
+function test() {
+
+    alert("Hello")
+}
+
+function RenderStudents(){
+    let ddl_students= document.getElementById('ddl_students')
+    fetch('https://test-api-8tdd.onrender.com/students')
+      .then(response => response.json())
+      .then(json => console.log(json))
+}
