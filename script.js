d46fc546093e1f3deac76cbebf5aae652e0e1a3a
CS <chadi.samadi96@gmail.com>
2024-07-09 00:10:25 +0300

    fixing script position

--- script.js
+++ script.js
@@ -7,7 +7,15 @@ function RenderStudents(){
     let ddl_students= document.getElementById('ddl_students')
     fetch('https://test-api-8tdd.onrender.com/students')
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(data=> {
+        data.forEach(student => {
+            let option = document.createElement('option');
+            option.innerHTML= student.name;
+            ddl_students.appendChild(option);
+            
+        });
+
+      })
 }
 
 RenderStudents();
